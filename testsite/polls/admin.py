95e0edf6d7c6a9536d08769c852341556c2e334a
Liu Ho Man <hmliu6@gmail.com>
2017-08-05 22:12:07 +0800

    Create new page

--- testsite/polls/admin.py
+++ testsite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import question, choice
+
+admin.site.register(question)
+admin.site.register(choice)
--- testsite/polls/models.py
+++ testsite/polls/models.py
@@ -2,3 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class question(models.Model):
+
+    questionText = models.CharField(max_length=200)
+    pubDate = models.DateTimeField('Date published')
+
+    def __str__(self):
+        return self.questionText
+
+class choice(models.Model):
+
+    choiceText = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choiceText
