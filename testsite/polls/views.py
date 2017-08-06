5eef3585e661bb18889e1600bef8564711aa26ee
Liu Ho Man <hmliu6@gmail.com>
2017-08-06 17:26:17 +0800

    New commit

--- testsite/polls/views.py
+++ testsite/polls/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import question
 
 def index(request):
-    return HttpResponse('Great Test.')
+    latest_question = question.objects.order_by('-pubDate')[:5]
+    output = ", ".join(q.questionText for q in latest_question)
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse('Question detail %s' % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse('Question result %s' % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('Vote on question %s' % question_id)
